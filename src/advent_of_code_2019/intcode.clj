03f2ac52c338a195b75b064c23e8124d70586528
Jason Kapp <mtbkapp@gmail.com>
2019-12-20 16:00:36 -0700

    why u not work yet!!!!!

--- src/advent_of_code_2019/intcode.clj
+++ src/advent_of_code_2019/intcode.clj
@@ -1,11 +1,13 @@
 (ns advent-of-code-2019.intcode
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [clojure.test :refer :all]))
 
 
 (defn wrap-int-bool
   [f]
   (fn [x y]
-    (bool->int (f x y))))
+    (if (f x y) 1 0)))
 
 (def opcodes
   {"01" {:op + :param-count 3 :op-type :binary} 
@@ -105,10 +107,10 @@
                           [(get-param program rel-ptr a0 m0)
                            (get-param program rel-ptr a1 m1)
                            dest])
-    (= :jump op-type) (let [[mode] param-modes
-                            [a0 jump-ptr] args]
-                        [(get-param program rel-ptr a0 mode)
-                         jump-ptr])
+    (= :jump op-type) (let [[m0 m1] param-modes
+                            [a0 a1] args]
+                        [(get-param program rel-ptr a0 m0)
+                         (get-param program rel-ptr a1 m1)])
     (= :op/output opc) [(get-param program
                                    rel-ptr
                                    (first args)
@@ -145,17 +147,14 @@
       (is (= [97 99 0]
              (read-params prg 1 (parse-op prg 0))))))
   (testing "jump ops"
-    (let [prg [5 0 876 99]]
-      (is (= [5 876]
+    (let [prg [5 0 3 99]]
+      (is (= [5 99]
              (read-params prg 1 (parse-op prg 0)))))
-    (let [prg [105 0 876 99]]
+    (let [prg [1105 0 876 99]]
       (is (= [0 876]
              (read-params prg 1 (parse-op prg 0)))))
-    (let [prg [105 0 876 99]]
-      (is (= [0 876]
-             (read-params prg 1 (parse-op prg 0)))))
-    (let [prg [205 2 876 99]]
-      (is (= [99 876]
+    (let [prg [2205 -1 2 99]]
+      (is (= [2205 99]
              (read-params prg 1 (parse-op prg 0))))))
   (testing "output"
     (let [prg [4 2 99]]
@@ -183,18 +182,38 @@
              (read-params prg 1 (parse-op prg 0)))))))
 
 
-(defn update-instr-ptr
-  [computer delta]
-  )
+(defn expand-memory
+  [computer max-i]
+  (update computer
+          :program
+          (fn [prg]
+            (if (< max-i (count prg))
+              prg
+              (into prg (repeat (- (inc max-i) (count prg)) 0))))))
+
+
+(deftest test-expand-memory
+  (let [c {:program [1 1 1]}]
+    (is (= c (expand-memory c 0))) 
+    (is (= c (expand-memory c 2)))
+    (is (= [1 1 1 0] (:program (expand-memory c 3))))
+    (is (= [1 1 1 0 0 0 0 0 0 0 0] (:program (expand-memory c 10))))))
+
 
 (defn write
   [computer dest v]
-  ; TODO expand memory when needed
-  (update computer
-          :program 
-          assoc
-          dest
-          v))
+  (-> (expand-memory computer dest)
+      (update :program 
+              assoc
+              dest
+              v)))
+
+
+(deftest test-write
+  (is (= [2 1 1] (:program (write {:program [1 1 1]} 0 2))))
+  (is (= [1 1 2] (:program (write {:program [1 1 1]} 2 2))))
+  (is (= [1 1 1 0 0 0 0 0 0 0 100] (:program (write {:program [1 1 1]} 10 100)))))
+
 
 (def next-state* nil)
 (defmulti next-state*
@@ -211,13 +230,14 @@
 
 (defn int->bool
   [x]
-  (if (zero? x) 0 1))
+  (if (zero? x) false true))
 
 (defn should-jump?
   [opc jump?]
   (if (= opc :op/jump-if)
     (int->bool jump?)
     (not (int->bool jump?))))
+
 
 (defmethod next-state* :jump
   [computer {opc :op [jump? location] :params :as op}]
@@ -226,11 +246,11 @@
     (update computer :instr-ptr + 3)))
 
 (defmethod next-state* :io
-  [computer {opc :op [output] :params :as op}]
+  [computer {opc :op [location] :params :as op}]
   (update
     (if (= opc :op/input)
-      (assoc computer :state :state/need-input)
-      (assoc computer :state :state/output :output output))
+      (assoc computer :state :state/need-input :input location)
+      (assoc computer :state :state/output :output location))
     :instr-ptr
     +
     2))
@@ -318,21 +338,294 @@
                    4
                    0)))
   (testing "jump ops"
-    
-    )
+    (testing "jump if"
+      (check-state (next-state (new-computer [1105 1 87 99]))
+                   [1105 1 87 99]
+                   :state/running
+                   87
+                   0)
+      (check-state (next-state (new-computer [1105 0 87 99]))
+                   [1105 0 87 99]
+                   :state/running
+                   3
+                   0))
+    (testing "jump if not"
+      (check-state (next-state (new-computer [1106 1 87 99]))
+                   [1106 1 87 99]
+                   :state/running
+                   3 
+                   0)
+      (check-state (next-state (new-computer [1106 0 87 99]))
+                   [1106 0 87 99]
+                   :state/running
+                   87
+                   0)))
   (testing "input"
-    
-    )
+    (let [prg [3 987]
+          {:keys [input] :as s} (next-state (new-computer prg))]
+      (check-state s prg :state/need-input 2 0)
+      (is (= 987 input))))
   (testing "output"
-    
-    )
+    (let [prg [4 0]
+          {:keys [output] :as s} (next-state (new-computer prg))]
+      (check-state s prg :state/output 2 0)
+      (is (= 4 (:output s))))
+    (let [prg [104 8889]
+          {:keys [output] :as s} (next-state (new-computer prg))]
+      (check-state s prg :state/output 2 0)
+      (is (= 8889 (:output s))))
+    (let [prg [204 3 0 77]
+          {:keys [output] :as s} (next-state (new-computer prg))]
+      (check-state s prg :state/output 2 0)
+      (is (= 77 (:output s)))))
   (testing "rel ptr set"
-    ; rel-ptr = param
-    ; or
-    ; rel-ptr = rel-ptr + param
-    ))
+    (check-state (next-state (new-computer [109 20]))
+                 [109 20]
+                 :state/running
+                 2
+                 20)
+    (check-state (next-state (new-computer [209 2 89]))
+                 [209 2 89]
+                 :state/running
+                 2
+                 89)
+    (check-state (next-state (new-computer [9 0]))
+                 [9 0]
+                 :state/running
+                 2
+                 9)))
 
 
 (defn run
-  [computer]
-  ) 
+  [{:keys [state] :as computer}]
+  (if (= :state/running state)
+    (recur (next-state computer))
+    computer))
+
+
+(deftest test-run
+  (testing "return on halt"
+    (check-state (run (new-computer [109 2 1101 3 4 0 99]))
+                 [7 2 1101 3 4 0 99]
+                 :state/halted
+                 6 
+                 2))
+  (testing "return on need input"
+    (check-state (run (new-computer [109 2 1101 3 4 0 3 0 99]))
+                 [7 2 1101 3 4 0 3 0 99]
+                 :state/need-input
+                 8 
+                 2))
+  (testing "return on output"
+    (check-state (run (new-computer [109 2 1101 3 4 0 4 0 99]))
+                 [7 2 1101 3 4 0 4 0 99]
+                 :state/output
+                 8 
+                 2)))
+
+
+(defn resume-with-input
+  [{s :state location :input :as computer} value]
+  (assert (= s :state/need-input))
+  (-> computer
+      (update :program assoc location value)
+      (dissoc :input)
+      (assoc :state :state/running)
+      (run)))
+
+(deftest test-resume-with-input
+  (check-state (-> (new-computer [109 2 3 0 99])
+                   (run)
+                   (resume-with-input 87))
+               [87 2 3 0 99]
+               :state/halted
+               4
+               2))
+
+
+(defn get-output
+  [{:keys [state output] :as computer}]
+  (assert (= state :state/output))
+  output)
+
+
+(defn resume-from-output
+  [{:keys [state] :as computer}]
+  (-> computer
+      (dissoc :output)
+      (assoc :state :state/running)
+      (run)))
+
+
+(deftest test-output
+  (let [s0 (run (new-computer [3 0 4 0 99]))
+        s1 (resume-with-input s0 89)
+        out (get-output s1)
+        s2 (resume-from-output s1)]
+    (check-state s0 [3 0 4 0 99] :state/need-input 2 0)
+    (check-state s1 [89 0 4 0 99] :state/output 4 0)
+    (check-state s2 [89 0 4 0 99] :state/halted 4 0)
+    (is (= 89 out))))
+
+
+(defn parse-program
+  [prg]
+  (into []
+        (comp (map string/trim)
+              (map #(Long/valueOf %)))
+        (string/split prg #",")))
+
+
+(defn read-program
+  [file]
+  (parse-program (slurp (io/resource file))))
+
+
+(deftest test-day2-examples
+  (testing "step by step example"
+    (let [s0 (next-state (new-computer (parse-program "1,9,10,3,2,3,11,0,99,30,40,50")))
+          s1 (next-state s0)
+          s2 (next-state s1)]
+      (check-state s0
+                   [1 9 10 70 2 3 11 0 99 30 40 50]
+                   :state/running
+                   4
+                   0)
+      (check-state s1
+                   [3500 9 10 70 2 3 11 0 99 30 40 50]
+                   :state/running
+                   8
+                   0)
+      (check-state s2
+                   [3500 9 10 70 2 3 11 0 99 30 40 50]
+                   :state/halted
+                   8
+                   0)))
+  (testing "fourth short example"
+    (check-state (run (new-computer [1 1 1 4 99 5 6 0 99]))
+                 [30 1 1 4 2 5 6 0 99]
+                 :state/halted
+                 8
+                 0))
+  (testing "part 1"
+    (let [prg (-> (read-program "day_02_part1.txt")
+                  (assoc 1 12)
+                  (assoc 2 2))
+          {:keys [program state]} (run (new-computer prg))]
+      (is (= 3306701 (first program)))
+      (is (= :state/halted state))))
+  (testing "part 2"
+    (let [prg (-> (read-program "day_02_part1.txt")
+                  (assoc 1 76)
+                  (assoc 2 21))
+          {:keys [program state]} (run (new-computer prg))]
+      (is (= 19690720 (first program)))
+      (is (= :state/halted state)))))
+
+
+(defn wrap-collect-output
+  ([computer] (wrap-collect-output computer []))
+  ([{:keys [state output] :as computer} coll]
+   (case state
+     :state/halted coll
+     :state/output (recur (resume-from-output computer) (conj coll output))
+     :state/running (recur (run computer) coll)
+     (throw (ex-info "Shouldn't be here" computer)))))
+
+(deftest test-day5
+  (testing "part1"
+    (is (= 7566643 (-> (new-computer (read-program "day_05.txt"))
+                       (run)
+                       (resume-with-input 1)
+                       (wrap-collect-output)
+                       (last)))))
+  (testing "part2"
+    (let [run-part2 (fn [program input]
+                      (-> (new-computer program)
+                          (run)
+                          (resume-with-input input)
+                          (wrap-collect-output)))]
+      (testing "compare examples"
+        (is (= [0] (run-part2 (parse-program "3,9,8,9,10,9,4,9,99,-1,8") 7)))
+        (is (= [1] (run-part2 (parse-program "3,9,8,9,10,9,4,9,99,-1,8") 8)))
+        (is (= [0] (run-part2 (parse-program "3,3,1107,-1,8,3,4,3,99") 8)))
+        (is (= [1] (run-part2 (parse-program "3,3,1107,-1,8,3,4,3,99") 7))))
+      (testing "jump examples"
+        (let [prg (parse-program "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9")]
+          (is (= [0] (run-part2 prg 0)))
+          (is (= [1] (run-part2 prg 1)))
+          (is (= [1] (run-part2 prg 2)))
+          (is (= [1] (run-part2 prg -1)))))
+      (testing "last example"
+        (is (= [999] (run-part2 (parse-program "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99")
+                                7)))
+        (is (= [1000] (run-part2 (parse-program "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99")
+                                 8)))
+        (is (= [1001] (run-part2 (parse-program "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99")
+                                 9))))) 
+    (is (= 9265694 (-> (new-computer (read-program "day_05.txt"))
+                       (run)
+                       (resume-with-input 5)
+                       (wrap-collect-output)
+                       last)))))
+
+
+(defn straight-amp
+  [program phase-settings]
+  (reduce (fn [input setting]
+            (-> (new-computer program)
+                (run)
+                (resume-with-input setting)
+                (resume-with-input input)
+                (get-output)))
+          0
+          phase-settings))
+
+
+
+(deftest test-day7
+  (testing "part1, last example"
+    (is (= 43210 (straight-amp (parse-program "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0")
+                               [4 3 2 1 0])))
+    (is (= 54321 (straight-amp (parse-program "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0")
+                               [0 1 2 3 4])))
+    (is (= 65210 (straight-amp (parse-program "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0")
+                               [1 0 4 3 2]))))
+  
+  )
+
+
+
+(comment
+  (straight-amp (parse-program "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0")
+                [1 0 4 3 2]
+                
+                )
+  (-> (parse-program "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0")
+      (new-computer)
+      (run)
+      (resume-with-input 0)
+      (resume-with-input 4321)
+      )
+
+  ;A, 3
+  ;B, 43
+  ;C, 432
+  ;D, 4321
+
+
+  (prn (-> (read-program "day_09.txt")
+           (new-computer)
+           (run)
+           (resume-with-input 1)
+           (wrap-collect-output)
+           ))
+ ; day9 part 1 is not 203!
+  )
+
+
+
+
+; day 5 says that, "Parameters that an instruction writes to will never be in immediate mode"
+
+
