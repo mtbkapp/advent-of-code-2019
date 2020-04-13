e27725ba5ec7309c44fe52e4529ae7bc5b5b7cdb
Jason Kapp <mtbkapp@gmail.com>
2020-04-13 17:26:40 -0600

    solution

--- src/advent_of_code_2019/day_13.clj
+++ src/advent_of_code_2019/day_13.clj
@@ -1,14 +1,17 @@
 (ns advent-of-code-2019.day-13
   (:require [advent-of-code-2019.intcode :as intcode]
             [clojure.test :refer :all]
-            [clojure.spec.alpha :as spec])
+            [clojure.spec.alpha :as spec]
+            [clojure.java.io :as io]
+            [clojure.core.async :as async])
   (:import [com.googlecode.lanterna.terminal DefaultTerminalFactory]
            [java.io Closeable]
            [com.googlecode.lanterna
             SGR
             TextColor
             TextColor$ANSI
-            TextCharacter]
+            TextCharacter
+            TerminalSize]
            [com.googlecode.lanterna.input KeyStroke KeyType]
            [com.googlecode.lanterna.screen TerminalScreen]))
 
@@ -70,19 +73,6 @@
                [0 0])))
 
 
-(defprotocol GameIO
-  (put-tile [_ x y tile])
-  (get-input [_]))
-
-
-(comment
-  (def start (intcode/new-computer (intcode/read-program "day_13.txt")))
-  (intcode/next-state (intcode/next-state start))
-  
-
-
-  )
-
 (defn add-quarters
   [computer]
   (update computer :program assoc 0 2))
@@ -105,12 +95,20 @@
   )
 
 
+(defn put-msg
+  [screen x y msg]
+  (-> (.newTextGraphics screen)
+      (.putString x y msg)))
+
+(def last-score (atom nil))
+
+
 (defn put-tile
   [screen x y tile]
-  (if (= tile 4) (prn "paddle!" x y))
   (if (= [-1 0] [x y])
-    (prn "score: " tile)
-    (.setCharacter screen x y (gc tile) #_(get tile-char-mapping tile))))
+    (do (put-msg screen 45 1 (str "Score: " tile))
+        (reset! last-score tile)) 
+    (.setCharacter screen x y (get tile-char-mapping tile))))
 
 
 ; init game
@@ -147,3 +145,158 @@
       (finally
         (.stopScreen screen)
         (.close term)))))
+
+
+(defmacro with-screen
+  [[screen cols rows rate] & body]
+  `(let [term# (.createTerminal (doto (DefaultTerminalFactory.)
+                                  (.setInitialTerminalSize (TerminalSize. ~cols ~rows))))
+         ~screen (TerminalScreen. term#)]
+     (try
+       (.startScreen ~screen)
+       ~@body
+       (finally
+         (.stopScreen ~screen)
+         (.close term#)))))
+
+
+
+
+(defn get-input
+  [screen]
+  (if-let [stroke (.pollInput screen)]
+    (let [t (.getKeyType stroke)]
+      (cond (= t KeyType/ArrowRight) 1
+            (= t KeyType/ArrowLeft) -1 
+            :else 0))
+    0))
+
+
+(comment
+  (def prgs (->> (slurp "cpu2")
+                 (clojure.string/split-lines)
+                 (map #(clojure.edn/read-string %))
+                 (map :program)
+                 (map vec)))
+
+
+  (doseq [[xs ys] (partition 2 1 prgs)]
+    (prn (diff-states xs ys))
+    )
+
+  (defn diff-states
+    [xs ys]
+    (reduce (fn [diffs i]
+              (let [x (get xs i)
+                    y (get ys i)]
+                (if (= x y)
+                  diffs
+                  (conj diffs [i x y]))))
+            []
+            (range (max (count xs) (count ys)))))
+  
+
+  )
+
+
+
+(comment
+
+  (def term (.createTerminal (doto (DefaultTerminalFactory.)
+                               (.setInitialTerminalSize (TerminalSize. 42 25)))))
+  (type term)
+
+  (.bell term)
+  (.getTerminalSize term)
+  (.close term)
+
+  (defn get-key
+    [screen]
+    (if-let [s (.pollInput screen)]
+      (.getKeyType s)))
+
+
+  (with-open [wr (io/writer "prog.edn")]
+    (clojure.pprint/pprint
+      (intcode/read-program "day_13.txt")
+      wr))
+
+
+
+  (with-screen
+    [screen 80 40]
+    (.readInput screen)
+    (loop [{:keys [state] :as cpu} (-> (clojure.edn/read-string (slurp "prog.edn")) 
+                                       (intcode/new-computer)
+                                       (add-quarters))
+           output-buff []
+           ticks 0]
+      (case state
+        :state/need-input (do (.refresh screen)
+                              (recur (intcode/next-with-input cpu (get-input screen))
+                                     output-buff
+                                     (inc ticks)))
+        :state/output (let [next-buff (conj output-buff (intcode/get-output cpu))
+                            next-cpu (intcode/next-from-output cpu)]
+                        (if (= 3 (count next-buff))
+                          (do (apply put-tile screen next-buff)
+                              (recur next-cpu
+                                     []
+                                     (inc ticks)))
+                          (recur next-cpu
+                                 next-buff
+                                 (inc ticks))))
+        :state/running (recur (intcode/next-state cpu)
+                              output-buff
+                              (inc ticks))
+        :state/halted  (put-msg screen 45 2 "HALT!")))
+    (.readInput screen))
+
+
+  (def prgs (->> (slurp "cpu2")
+                 (clojure.string/split-lines)
+                 (map #(clojure.edn/read-string %))
+                 (map :program)))
+
+
+  (doseq [[xs ys] (partition 2 1 prgs)]
+    (prn (diff-states xs ys))
+    )
+
+  
+
+
+  (defn diff
+    [xs ys]
+    (assert (= (count xs) (count ys)))
+    (->> (map (fn [i x y] [i (= x y)]) (range (count xs)) xs ys)
+         (filter (fn [[i eq?]] (not eq?)))
+         ))
+
+
+  (diff (first prgs)
+        (second prgs)
+        )
+
+
+  (map (fn [[xs ys]] [(count xs) (count ys)]) (partition 2 1 prgs))
+
+
+  (require 'clojure.data)
+
+  (doseq [[prev state] (partition 2 1 prgs)]
+    (prn (second (clojure.data/diff prev state)))
+    )
+
+
+
+  ; score = 21415
+
+  
+
+  
+ 
+    
+
+
+  )
